3e2a7f727c0ecca1f0e32f18f692a5383e9bf07f
raedaddala <addala.raed@gmail.com>
2023-11-13 22:02:48 +0100

    .

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from chain import Chain
 
 
 def main():
-    chain = Chain(8)
+    chain = Chain(18)
     while True:
         data = input("Add something to the chain: ")
         chain.add_to_pool(data)
         chain.mine()
-        
+
+
 if __name__ == "__main__":
     main()
